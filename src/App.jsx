1bfd903c1074cdcdd66c9cfcf8db0023af3c65f3
AnaJromero <anajoseromero10@gmail.com>
2023-07-14 21:08:31 -0400

    Creando Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components.jsx/Navbar"
+
+const App = () => {
+
+  let nombre = "Ana" 
+  return (
+    <>
+    <h1>Bienvenida{nombre}</h1>
+    </>
+  )
+}
+
+export default App  
